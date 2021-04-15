46115af54a6c70e75a49e73ab7d4068bda0b3ec3
indraworks <indra.works2020@gmail.com>
2021-04-15 16:23:18 +0700

    update counter

--- lib/src/screens/counter_home_screen.dart
+++ lib/src/screens/counter_home_screen.dart
@@ -1,57 +1,178 @@
-
-
 import 'package:flutter/material.dart';
 
+//kita refactoring isi dari code,nama class sama
+//sesuai dgn nama file dartnya
+
+//manual
+
 class MeetuperApp extends StatelessWidget {
-  final String appTitle = " Meetuper App";
+  final String appTitle = "Meetuper App";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        //theme.home.body
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: CounterHomeScreen(
-          title: appTitle,
-        ));
+        home: CounterHomeScreen(title: appTitle));
   }
 }
 
-class CounterHomeScreen extends StatelessWidget {
-  final String _title; //variable intansiate
+class CounterHomeScreen extends StatefulWidget {
+  final String _title;
   //constructornya
+  //
   CounterHomeScreen({String title}) : _title = title;
 
-  //function
+  // CounterHomeScreenState createState() => CounterHomeScreenState();
+  //  yg diatas sama syntax commandnya dgn func () { return statement}
+  @override
+  CounterHomeScreenState createState() {
+    return CounterHomeScreenState();
+  }
+}
+
+class CounterHomeScreenState extends State<CounterHomeScreen> {
+  int _counter = 0;
   _increment() {
-    print('i got clicked by touch!');
+    //set state ada dlm sbuah sunction yg dipanggil oleh widget
+    //sgai referencenya dlm hal ini adala floatingbutton
+    setState(() {
+      _counter++; //setState jlankan function ubah counter =counter+1
+      //dari sini setState  re-build widget buildCOntext dan prbarui rendernya
+      //tiap2 counter brubah atau state brubah maka rebuild widget!
+    });
   }
 
   Widget build(BuildContext context) {
-    //returnnya scafold
+    print('caling build widget');
     return Scaffold(
+      appBar: AppBar(centerTitle: true, title: Text('Meetuper')),
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Welcome in $_title lets increment number',
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 15.0),
-          ),
-          Text(
-            'Click Counter : 0',
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 30.0),
-          )
-        ],
-      )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to our ${widget._title},lets increment number!', //karna _title ada diluar class
+              //dluar class dan berada di statefullwidget
+              //kita pake widget utk ambil variable ttitlenya,
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 15.0),
+            ),
+            Text('Clik Counter : $_counter ',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontSize: 30.0))
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: _increment,
       ),
-      appBar: AppBar(
-        centerTitle: true, //buat nengahin text
-        title: Text(_title),
-      ),
     );
   }
 }
+
+/*
+tentang perubahan state nah jadi kalau mau ubah itu 
+harus setting setState dan dia dipassing dalam sbuah function 
+setState(){
+
+}
+nah setState ini akan menjalankan widget yaitu mmbuat context widget lagi 
+atau build dan merender perubahan tadi ,jadi stiap ada perubahan setState
+maka dia akan buildWidget
+
+
+
+*/
+
+//ini dari stf sniper ktik stf artinya statfull widget automatis
+//akan trbuat tiggal ganti nama function/classnya
+// class CounterHomeScreen extends StatefulWidget {
+//   @override
+//   _CounterHomeScreenState createState() => _CounterHomeScreenState();
+// }
+
+// class _CounterHomeScreenState extends State<CounterHomeScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+
+//     );
+//   }
+// }
+
+//old stateless widget
+// class MeetuperApp extends StatelessWidget {
+//   final String appTitle = " Meetuper App";
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         //theme.home.body
+//         theme: ThemeData(primarySwatch: Colors.blue),
+//         home: CounterHomeScreen(
+//           title: appTitle,
+//         ));
+//   }
+// }
+
+// class CounterHomeScreen extends StatelessWidget {
+//   final String _title; //variable intansiate
+//   int _counter = 0;
+//   //constructornya
+//   CounterHomeScreen({String title}) : _title = title;
+
+//   //function
+//   _increment() {
+//     print('i got clicked by touch!');
+//     _counter++;
+//     return _counter;
+//   }
+
+//   Widget build(BuildContext context) {
+//     //returnnya scafold
+//     return Scaffold(
+//       body: Center(
+//           child: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Text(
+//             'Welcome in $_title lets increment number',
+//             textDirection: TextDirection.ltr,
+//             style: TextStyle(fontSize: 15.0),
+//           ),
+//           Text(
+//             'Click Counter : $_counter',
+//             textDirection: TextDirection.ltr,
+//             style: TextStyle(fontSize: 30.0),
+//           )
+//         ],
+//       )),
+//       floatingActionButton: FloatingActionButton(
+//         //floating dalam scafold
+//         child: Icon(Icons.add),
+//         onPressed: _increment,
+//       ),
+//       appBar: AppBar(
+//         centerTitle: true, //buat nengahin text
+//         title: Text(_title),
+//       ),
+//     );
+//   }
+// }
+
+/*
+KITA BUAT STATEFULL WIDGET dari stateless sebab ada variable yg akanberubah 
+nilai nilainya maka adari itu kita refactoring codenya
+penjelasan : 
+
+
+*/
+
+/*
+kita barusan pindah class counterhome screen dari menutama 
+kalau mau lihat klikkanann uk import package yg tidak ada 
+pilih import
+
+
+
+
+*/
